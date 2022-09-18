f9849b21c25a9b0a37ca6e93b7c6df25fbb15af0
brunosousa24 <brunosousa_24@hotmail.com>
2022-09-18 12:43:35 +0100

    file structure

--- DogsApp/View/DogBreedImagesPage.swift
+++ DogsApp/View/DogBreedImagesPage.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class DogBreedImagesPage: UIViewController {
     
+    private var _dogBreedListPage : DogBreedListPage?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Dog Breed Images";
+        _dogBreedListPage = tabBarController?.viewControllers?[1] as? DogBreedListPage
         
         APICaller().getBreedsData { dogBreedList in
             
---
+++ DogsApp/View/DogBreedListPage.swift
@@ -0,0 +1,13 @@
+//
+//  DogBreedListPage.swift
+//  DogsApp
+//
+//  Created by Bruno Sousa on 18/09/2022.
+//
+
+import UIKit
+
+class DogBreedListPage: UIViewController {
+    
+    
+}
